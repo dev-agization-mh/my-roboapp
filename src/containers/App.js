ae42609804ac6e1bc4254ccadb041b57cb70dab4
Michael <41684436+dev-agization-mh@users.noreply.github.com>
2018-08-01 18:25:53 +0200

    Redux instead of state

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,16 +1,29 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
 import ErrorBoundary from '../components/ErrorBoundary';
 import './App.css';
+import { setSearchField } from '../actions.js';
+
+const mapStateToProps = state => {
+    return {
+        searchField: state.searchField
+    }
+}
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        onSearchChange: (event) => dispatch(setSearchField(event.target.value))
+    }
+}
 
 class App extends React.Component {
     constructor() {
         super();
         this.state = {
-            robots: [],
-            searchfield: ''
+            robots: []
         }
     }
 
@@ -20,24 +33,17 @@ class App extends React.Component {
             .then(users => this.setState( {robots: users} ));
     }
 
-    onSearchChange = (event) => {
-        this.setState({ searchfield: event.target.value });
-    }
-
-    onCardClick = (event) => {
-        alert('hello');
-    }
-
     render() {
-        const { robots, searchfield } = this.state;
+        const { robots } = this.state;
+        const { searchField, onSearchChange } = this.props;
         const filteredRobots = robots.filter(robot => {
-            return robot.name.toLowerCase().includes(searchfield.toLowerCase());
+            return robot.name.toLowerCase().includes(searchField.toLowerCase());
         })
         if (!robots.length) {
             return (
                 <div className='tc'>
                     <h1 className='MainHeader'>Robo Friends</h1>
-                    <SearchBox searchChange={this.onSearchChange} />
+                    <SearchBox searchChange={onSearchChange} />
                     <h2 className='white'>Loading...</h2>
                 </div>
             )
@@ -47,7 +53,7 @@ class App extends React.Component {
                 <div className='tc'>
                     <div className='Top'>
                     <h1 className='MainHeader'>Robo Friends</h1>
-                    <SearchBox searchChange={this.onSearchChange} />
+                    <SearchBox searchChange={onSearchChange} />
                     </div>
                     <Scroll>
                         <ErrorBoundary>
@@ -62,4 +68,4 @@ class App extends React.Component {
  
 }
 
-export default App;
+export default connect(mapStateToProps, mapDispatchToProps)(App);
